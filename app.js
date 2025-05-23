770de61e8e1333b1c652be9d55f8ea741405414d
kseniakopteva <ksenija.kopteva@gmail.com>
2025-05-23 19:16:18 +0300

    refactored, commented and fixed favicon

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const questionText = document.getElementById("question-text")
 const optionContainer = document.getElementById("option-container")
 const progressBar = document.getElementById("progress-bar")
 const errorContainer = document.getElementById("error-container")
+const questionBlock = document.getElementById("question-block")
+const resultBlock = document.getElementById("test-result-block")
+const introBlock = document.getElementById("introduction-block")
+const GSlen = 15 // Godspeed testa vienību skaits
+const IDAQlen = 21 // IDAQ testa vienību skaits
 
 let questionCounter = 0
 let currentQuestion
@@ -13,18 +18,21 @@ let chosenOption = null
 let GS = []
 let IDAQ = []
 
-let medianGS = 0
-let medianIDAQ = 0
-let median = 0
+let averageGS = 0
+let averageIDAQ = 0
+let totalAverage = 0
 
 let resultMessage = ""
 let resultRating = ""
 
 function setQuestions() {
     const totalQ = test.length
+
+    // aizpildam masīvu ar visiem jautājumiem
     for (let i = 0; i < totalQ; i++) {
         availableQuestions.push(test[i])
     }
+    // aizpildam progresa joslu
     let otherElem = document.createElement("div")
     otherElem.innerHTML = ""
     for (let i = 0; i < availableQuestions.length; i++) {
@@ -32,29 +40,31 @@ function setQuestions() {
     }
 }
 
-// set number, text and answer options
+// iestatām jautājuma numuru, tekstu, atbilžu variantus un attēlu, ja ir
 function getNewQuestion() {
     document.getElementById("question-img").innerHTML = ""
     chosenOption = null
     errorContainer.innerHTML = ""
     questionNumber.innerHTML = questionCounter + 1 + "/" + test.length
-    // random question
+
+    // nejaušs jautājums
     const questionObj = availableQuestions[Math.floor(Math.random() * availableQuestions.length)]
     currentQuestion = questionObj
     questionText.innerHTML = currentQuestion.q
 
-    // if there is an image
+    // ja ir attēls
     if (currentQuestion.img != null) {
-        // add img element
+        // pievienojam attēlu
         const img = document.createElement("img")
         img.setAttribute("src", currentQuestion.img)
         document.getElementById("question-img").appendChild(img)
     }
 
+    // nodzēšam jau parādīto jautājumu no pieejamiem jautājumiem
     const id = availableQuestions.indexOf(questionObj)
-    // remove it
     availableQuestions.splice(id, 1)
 
+    // pievienojam atbilžu variantus
     const totalOpt = currentQuestion.options.length
     availableOptions = []
     for (let i = 0; i < totalOpt; i++) {
@@ -73,7 +83,7 @@ function getNewQuestion() {
         option.setAttribute("onclick", "rememberResult(this)")
     }
 
-    // update the progress bar
+    // atjauninām progresa joslu
     progressBar.innerHTML = ""
     for (let i = 0; i < questionCounter; i++) {
         let completeElem = document.createElement("div")
@@ -85,7 +95,6 @@ function getNewQuestion() {
     completeElem.innerHTML = ""
     completeElem.className = "complete-last"
     progressBar.appendChild(completeElem)
-
     for (let i = questionCounter; i < test.length; i++) {
         let otherElem = document.createElement("div")
         otherElem.innerHTML = ""
@@ -95,22 +104,23 @@ function getNewQuestion() {
     questionCounter++
 }
 
+// ierakstām rezultātu masīvos
 function rememberResult(elem) {
     chosenOption = elem
-    let qNum = chosenOption.id.split("-")[1]
-    let oNum = chosenOption.id.split("-")[2]
+    let questionNum = chosenOption.id.split("-")[1]
+    let optionNum = chosenOption.id.split("-")[2]
 
     elem.setAttribute("onclick", "")
     for (let i = 1; i <= availableOptions.length; i++) {
-        if (i != oNum) {
-            document.getElementById("option-" + qNum + "-" + i).setAttribute("onclick", "rememberResult(this)")
+        if (i != optionNum) {
+            document.getElementById("option-" + questionNum + "-" + i).setAttribute("onclick", "rememberResult(this)")
         }
     }
 
-    if (qNum >= 1 && qNum <= 15) {
-        GS[qNum] = parseInt(oNum)
+    if (questionNum >= 1 && questionNum <= 15) {
+        GS[questionNum] = parseInt(optionNum)
     } else {
-        IDAQ[qNum] = parseInt(oNum)
+        IDAQ[questionNum - 15] = parseInt(optionNum)
     }
 
     errorContainer.innerHTML = ""
@@ -124,47 +134,46 @@ function rememberResult(elem) {
     elem.classList.add("chosen")
 }
 
+// ja tiek izvēlēts kāds no variantiem, parādām nākamo jautājumu
 function next() {
     if (chosenOption === null) {
-        showErrorMessage()
+        errorContainer.innerHTML = "Lūdzu, izvēlieties kādu no variantiem!"
         return
     }
     if (questionCounter === test.length) {
+        // ja jautājumi beidzās, parādām rezultātus
         end()
     } else {
         getNewQuestion()
     }
 }
 
-function showErrorMessage() {
-    errorContainer.innerHTML = "Lūdzu, izvēlieties kādu no variantiem!"
-}
-
 function start() {
-    document.getElementById("introduction-block").classList.add("hide")
-    document.getElementById("question-block").classList.remove("hide")
-    document.getElementById("progress-bar").classList.remove("hide")
+    introBlock.classList.add("hide")
+    questionBlock.classList.remove("hide")
+    progressBar.classList.remove("hide")
 }
 
+// analizējām rezultātus un ierakstām datus lapā
 function end() {
-    document.getElementById("question-block").classList.add("hide")
-    document.getElementById("test-result-block").classList.remove("hide")
-    document.getElementById("progress-bar").classList.add("hide")
+    questionBlock.classList.add("hide")
+    resultBlock.classList.remove("hide")
+    progressBar.classList.add("hide")
     analyseResults()
-    document.getElementById("IDAQ-score").innerHTML = medianIDAQ
-    document.getElementById("GS-score").innerHTML = medianGS
-    document.getElementById("final-score").innerHTML = median
+    document.getElementById("IDAQ-score").innerHTML = averageIDAQ
+    document.getElementById("GS-score").innerHTML = averageGS
+    document.getElementById("final-score").innerHTML = totalAverage
 
-    if (median <= 1) {
+    if (totalAverage <= 1) {
         resultRating = "ZEMS"
         resultMessage = "Jums nav izteikta tendence apzināti piešķirt digitālām ierīcēm cilvēkam līdzīgas īpašības; vienkārši sakot, Jūsu apzinātā uztverē digitālas ierīces ir tikai nedzīvi objekti -- nekas vairāk."
-    } else if (median > 1 && median <= 2.5) {
+    } else if (totalAverage > 1 && totalAverage <= 2.5) {
         resultRating = "VIDĒJS"
         resultMessage = "Jums ir vidēji izteikta tendence apzināti piešķirt digitālām ierīcēm cilvēkam līdzīgas īpašības; vairākumā gadījumu digitālas ierīces ir vienkārši nedzīvi objekti, taču dažreiz Jums šķiet, ka tās ir līdzīgas cilvēkiem."
-    } else if (median > 2.5 && median < 4) {
+    } else if (totalAverage > 2.5 && totalAverage < 4) {
         resultRating = "AUGSTĀKS PAR VIDĒJO"
         resultMessage = "Jums ir diezgan liela tendence apzināti piešķirt digitālām ierīcēm cilvēkam līdzīgas īpašības; daudzos gadījumos Jums šķiet, ka tām piemīt domas, nodomi, vērtības un dzīvums."
-    } else if (median >= 4) {
+    } else if (totalAverage >= 4) {
         resultRating = "AUGSTS"
         resultMessage = "Jums ir ļoti stipri izteikta tendence apzināti piešķirt digitālām ierīcēm cilvēkam līdzīgas īpašības; vairākumā gadījumos Jums šķiet, ka digitālas ierīces ir dzīvas, ar saviem nodomiem, domām un emocijām."
     }
@@ -173,24 +182,20 @@ function end() {
     document.getElementById("result-message").innerHTML = resultMessage
 }
 
+// aprēķinām vidējo katrā no masīviem un kopējo vidējo vērtību
 function analyseResults() {
-    // GS - 15
-    for (let i = 1; i <= 15; i++) {
-        medianGS += GS[i]
+    averageGS = Math.round((average(GSlen, GSlen, GS) + Number.EPSILON) * 100) / 100
+    averageIDAQ = Math.round((average(IDAQlen, IDAQlen, IDAQ) + Number.EPSILON) * 100) / 100
+
+    totalAverage = Math.round(((averageGS + averageIDAQ) / 2 + Number.EPSILON) * 100) / 100
+}
+
+function average(end, length, arr) {
+    let average = 0
+    for (let i = 1; i <= end; i++) {
+        average += arr[i]
     }
-    medianGS /= 15
-
-    // IDAQ - 21
-    for (let i = 16; i <= 36; i++) {
-        medianIDAQ += IDAQ[i]
-    }
-    medianIDAQ /= 21
-
-    medianGS = Math.round((medianGS + Number.EPSILON) * 100) / 100
-    medianIDAQ = Math.round((medianIDAQ + Number.EPSILON) * 100) / 100
-
-    median = (medianGS + medianIDAQ) / 2
-    median = Math.round((median + Number.EPSILON) * 100) / 100
+    return average / length
 }
 
 window.onload = function () {
